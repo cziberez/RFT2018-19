ccefab297289c463dfb284eeb35340b3b884069b
Czibere Zoltán <czibere.zoltan.adam@gmail.com>
2018-10-31 22:59:51 +0100

    ThemeSwitcher, omnifaces

---
+++ kaJava-web/src/main/java/hu/food/view/ThemeSwitcherView.java
@@ -0,0 +1,28 @@
+package hu.food.view;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import java.util.List;
+
+@ManagedBean
+public class ThemeSwitcherView {
+
+    private List<Theme> themes;
+
+    @ManagedProperty("#{themeService}")
+    private ThemeService service;
+
+    @PostConstruct
+    public void init() {
+        themes = service.getThemes();
+    }
+
+    public List<Theme> getThemes() {
+        return themes;
+    }
+
+    public void setService(ThemeService service) {
+        this.service = service;
+    }
+}
