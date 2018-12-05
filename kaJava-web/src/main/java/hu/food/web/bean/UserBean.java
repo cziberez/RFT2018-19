1bad457c80e1f1f4afb05513662715b2925d8cc7
Czibere Zoltán <czibere.zoltan.adam@gmail.com>
2018-12-06 00:03:35 +0100

    Visszavezetes

--- kaJava-web/src/main/java/hu/food/web/bean/UserBean.java
+++ kaJava-web/src/main/java/hu/food/web/bean/UserBean.java
@@ -1,77 +1,59 @@
 package hu.food.web.bean;
 
-import java.io.Serializable;
-import java.util.List;
+import hu.food.service.UserService;
+import hu.food.service.vo.AddressVo;
+import hu.food.service.vo.UserVo;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-
-import hu.food.service.UserService;
-import hu.food.service.vo.AddressVo;
-import hu.food.service.vo.UserVo;
+import java.io.Serializable;
 
 @Named("userBean")
 @ViewScoped
 public class UserBean implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@EJB
-	private UserService userService;
+    @EJB
+    private UserService userService;
 
-	private List<UserVo> users;
+    private UserVo userVo;
 
-	private UserVo user;
+    private AddressVo addressVo;
 
-	private AddressVo address;
+    @PostConstruct
+    public void init() {
+        userVo = new UserVo();
+        addressVo = new AddressVo();
+    }
 
-	@PostConstruct
-	public void init() {
-		users = userService.getAllUsers();
-		user = new UserVo();
-		setAddress(new AddressVo());
-	}
+    public void addUser() {
+        userService.saveNewUser(userVo, addressVo);
+    }
 
-	public void addUser() {
-		userService.addUser(user);
-	}
-	
-	public void SaveNewUser() {
-		userService.saveNewUser(user, address);
-	}
+    public UserService getUserService() {
+        return userService;
+    }
 
-	public UserService getUserService() {
-		return userService;
-	}
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
+    public UserVo getUserVo() {
+        return userVo;
+    }
 
-	public List<UserVo> getUsers() {
-		return users;
-	}
+    public void setUserVo(UserVo userVo) {
+        this.userVo = userVo;
+    }
 
-	public void setUsers(List<UserVo> users) {
-		this.users = users;
-	}
+    public AddressVo getAddressVo() {
+        return addressVo;
+    }
 
-	public UserVo getUser() {
-		return user;
-	}
-
-	public void setUser(UserVo user) {
-		this.user = user;
-	}
-
-	public AddressVo getAddress() {
-		return address;
-	}
-
-	public void setAddress(AddressVo address) {
-		this.address = address;
-	}
-
+    public void setAddressVo(AddressVo addressVo) {
+        this.addressVo = addressVo;
+    }
 }
