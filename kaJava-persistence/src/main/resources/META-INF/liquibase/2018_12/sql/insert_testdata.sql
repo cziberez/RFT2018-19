842d65d2ad334fc629a0ca765f68ac5ae171b9f5
Czibere Zoltán <czibere.zoltan.adam@gmail.com>
2018-12-17 00:01:40 +0100

    Finalize project

---
+++ kaJava-persistence/src/main/resources/META-INF/liquibase/2018_12/sql/insert_testdata.sql
@@ -0,0 +1,19 @@
+INSERT INTO "address"(id, moddate, recdate, status, city, door, floor, housenumber, postcode, street) VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, ACTIVE, Debrecen, 2, 3, 12, 4032, utca);
+INSERT INTO "user"(id, moddate, recdate, status, email, firstname, lastname, password, phonenumber, role, username, useraddress_id) VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, ACTIVE, admin@kajava.hu, Admin, Sanyi, admin, +36201234567, ADMINISTRATOR, admin, 1);
+INSERT INTO "user"(id, moddate, recdate, status, email, firstname, lastname, password, phonenumber, role, username, useraddress_id) VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, ACTIVE, guest@kajava.hu, Guest, Peti, admin, +36201234567, CUSTOMER, guest, 2);
+
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (1, null, null, null, 'Cigány Pecsenye', 'Cigány Pecsenye', 1799, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (2, null, null, null, 'Töltöttkáposzta', 'Töltöttkáposzta', 2532, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (3, null, null, null, 'Húsleves', 'Húsleves', 3381, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (4, null, null, null, 'Babgulyás', 'Babgulyás', 2322, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (5, null, null, null, 'Csokis deszert', 'Csokis deszert', 3168, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (1, null, null, null, 'Cigány Pecsenye', 'Cigány Pecsenye', 1799, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (2, null, null, null, 'Töltöttkáposzta', 'Töltöttkáposzta', 2532, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (3, null, null, null, 'Húsleves', 'Húsleves', 3381, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (4, null, null, null, 'Babgulyás', 'Babgulyás', 2322, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (5, null, null, null, 'Csokis deszert', 'Csokis deszert', 3168, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (1, null, null, null, 'Cigány Pecsenye', 'Cigány Pecsenye', 1799, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (2, null, null, null, 'Töltöttkáposzta', 'Töltöttkáposzta', 2532, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (3, null, null, null, 'Húsleves', 'Húsleves', 3381, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (4, null, null, null, 'Babgulyás', 'Babgulyás', 2322, null);
+insert into food (id, moddate, recdate, status, description, name, price, foodImage_id) values (5, null, null, null, 'Csokis deszert', 'Csokis deszert', 3168, null);
